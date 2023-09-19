19dc107453b2fb617e10de2e84b98d8cbb4ac9cc
Dave Pearson <davep@davep.org>
2023-09-19 12:01:44 +0100

    Start work on a sensible demo
    This will mimic the Plotext README, showing off the different kinds of
    plots. In doing so it'll also help me test that the library is working in a
    sensible way.

--- src/textual_plotext/__main__.py
+++ src/textual_plotext/__main__.py
@@ -9,79 +9,117 @@ $ python -m textual_plotext
 it will show a demonstration of the library in action.
 """
 
+from typing import Callable
+
 from textual.app import App, ComposeResult
-from textual.containers import Grid
-from textual.widgets import Header, Footer
+from textual.containers import VerticalScroll
+from textual.widgets import Header, Label, Rule, TabbedContent, TabPane
 
-from textual_plotext import PlotextPlot
+from textual_plotext import Plot, PlotextPlot
 
 
-class RefreshingPlot(PlotextPlot):
-    def update(self) -> None:
-        plt = self.plot
-        plt.clf()
-        l, frames = 1000, 30
-        x = range(1, l + 1)
-        y = plt.sin(periods=2, length=l, phase=2 * self.phase / frames)
-        plt.scatter(x, y, marker="braille")
-        plt.title("Updating plot!")
-        plt.theme("dark")
-        plt.ylim(-1, 1)
-        self.phase += 1
-        self.refresh()
+class ExamplesPane(VerticalScroll):
+    """Base class for a pane of examples."""
 
-    def on_mount(self) -> None:
-        self.phase = 0
-        self.update()
-        self.set_interval(0.1, self.update)
+    DEFAULT_CSS = """
+    ExamplesPane Label {
+        margin: 2;
+        width: 1fr;
+        text-align: center;
+    }
+    """
+
+    def make_a(self, example: Callable[[Plot], None]) -> PlotextPlot:
+        plot = PlotextPlot()
+        example(plot.plot)
+        return plot
+
+
+class BasicPane(ExamplesPane):
+    """Examples from the basic section of the Plotext documentation."""
+
+    def scatter_plot(self, plt: Plot) -> None:
+        """https://github.com/piccolomo/plotext/blob/master/readme/basic.md#scatter-plot"""
+        y = plt.sin()
+        plt.scatter(y)
+        plt.title("Scatter Plot")
+
+    def line_plot(self, plt: Plot) -> None:
+        """https://github.com/piccolomo/plotext/blob/master/readme/basic.md#line-plot"""
+        y = plt.sin()
+        plt.plot(y)
+        plt.title("Line Plot")
+
+    def log_plot(self, plt: Plot) -> None:
+        """https://github.com/piccolomo/plotext/blob/master/readme/basic.md#log-plot"""
+        l = 10**4
+        y = plt.sin(periods=2, length=l)
+        plt.plot(y)
+        plt.xscale("log")  # for logarithmic x scale
+        plt.yscale("linear")  # for linear y scale
+        plt.grid(0, 1)  # to add vertical grid lines
+        plt.title("Logarithmic Plot")
+        plt.xlabel("logarithmic scale")
+        plt.ylabel("linear scale")
+
+    def stem_plot(self, plt: Plot) -> None:
+        """https://github.com/piccolomo/plotext/blob/master/readme/basic.md#stem-plot"""
+        y = plt.sin()
+        plt.plot(y, fillx=True)
+        plt.title("Stem Plot")
+
+    def multiple_data_sets(self, plt: Plot) -> None:
+        """https://github.com/piccolomo/plotext/blob/master/readme/basic.md#multiple-data-sets"""
+        y1 = plt.sin()
+        y2 = plt.sin(phase=-1)
+        plt.plot(y1, label="plot")
+        plt.scatter(y2, label="scatter")
+        plt.title("Multiple Data Set")
+
+    def multiple_axes_plot(self, plt: Plot) -> None:
+        """https://github.com/piccolomo/plotext/blob/master/readme/basic.md#multiple-axes-plot"""
+        y1 = plt.sin()
+        y2 = plt.sin(2, phase=-1)
+        plt.plot(y1, xside="lower", yside="left", label="lower left")
+        plt.plot(y2, xside="upper", yside="right", label="upper right")
+        plt.title("Multiple Axes Plot")
+
+    def compose(self) -> ComposeResult:
+        """Compose the child widgets."""
+        yield Label(
+            "Examples from https://github.com/piccolomo/plotext/blob/master/readme/basic.md"
+        )
+        yield self.make_a(self.scatter_plot)
+        yield Rule()
+        yield self.make_a(self.line_plot)
+        yield Rule()
+        yield self.make_a(self.log_plot)
+        yield Rule()
+        yield self.make_a(self.stem_plot)
+        yield Rule()
+        yield self.make_a(self.multiple_data_sets)
+        yield Rule()
+        yield self.make_a(self.multiple_axes_plot)
+        yield Rule()
 
 
 class DemoApp(App[None]):
     """Demonstration application for the library."""
 
     CSS = """
-    Grid {
-        grid-size: 2;
-    }
-
     PlotextPlot {
-        border: solid cornflowerblue;
+        height: 40%
     }
     """
+
+    TITLE = "textual-plotext Demonstration"
 
     def compose(self) -> ComposeResult:
         """Compose the child widgets."""
         yield Header()
-        with Grid():
-            yield PlotextPlot(id="line")
-            yield RefreshingPlot()
-            yield PlotextPlot(id="logarithmic")
-            yield PlotextPlot(id="multiple")
-        yield Footer()
-
-    def on_mount(self) -> None:
-        plt = self.query_one("#line", PlotextPlot).plot
-        y = plt.sin()
-        plt.plot(y)
-        plt.title("Line Plot")
-
-        plt = self.query_one("#logarithmic", PlotextPlot).plot
-        l = 10**4
-        y = plt.sin(periods=2, length=l)
-        plt.plot(y)
-        plt.xscale("log")
-        plt.yscale("linear")
-        plt.grid(0, 1)
-        plt.title("Logarithmic Plot")
-        plt.xlabel("logarithmic scale")
-        plt.ylabel("linear scale")
-
-        plt = self.query_one("#multiple", PlotextPlot).plot
-        y1 = plt.sin()
-        y2 = plt.sin(2, phase=-1)
-        plt.plot(y1, xside="lower", yside="left", label="lower left")
-        plt.plot(y2, xside="upper", yside="right", label="upper right")
-        plt.title("Multiple Axes Plot")
+        with TabbedContent():
+            with TabPane("Basic Plots"):
+                yield BasicPane()
 
 
 if __name__ == "__main__":
