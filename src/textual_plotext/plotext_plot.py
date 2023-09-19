54a4d4eb87f7e6c148548e80f2ba2c95e8628ca3
Dave Pearson <davep@davep.org>
2023-09-19 10:38:29 +0100

    Add support for automatically setting a plot theme
    The developer can override this by turning off auto-theme-setting. But for
    the most part they probably want to have it be light or dark depending.
    
    Themes, automatic, for the people.
    
    (TODO: watch dark mode and toggle as necessary)

--- src/textual_plotext/plotext_plot.py
+++ src/textual_plotext/plotext_plot.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 from textual.app import RenderResult
+from textual.reactive import var
 from textual.widget import Widget
 
 from rich.text import Text
@@ -12,6 +13,15 @@ from .plot import Plot
 
 class PlotextPlot(Widget):
     """A Plotext plot display widget."""
+
+    auto_theme: var[bool] = var(True)
+    """Should the plot automatically adjust the theme depending on light and dark mode?"""
+
+    light_mode_theme: var[str] = var("default")
+    """The Plotext theme to use for light mode."""
+
+    dark_mode_theme: var[str] = var("dark")
+    """The Plotext theme to use for dark mode."""
 
     def __init__(
         self,
@@ -34,4 +44,18 @@ class PlotextPlot(Widget):
 
     def render(self) -> RenderResult:
         self.plot.plotsize(self.size.width, self.size.height)
+        if self.auto_theme:
+            self.plot.theme(
+                self.dark_mode_theme if self.app.dark else self.light_mode_theme
+            )
         return Text.from_ansi(self.plot.build())
+
+    def _watch_light_mode_theme(self) -> None:
+        """React to changes to the light mode theme."""
+        if self.auto_theme and not self.app.dark:
+            self.refresh()
+
+    def _watch_dark_mode_theme(self) -> None:
+        """React to changes to the dark mode theme."""
+        if self.auto_theme and self.app.dark:
+            self.refresh()
