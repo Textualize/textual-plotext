c7f87f070f057961f8afc0f2172ffc02ca0cec0b
Dave Pearson <davep@davep.org>
2023-09-20 15:31:07 +0100

    Import Plot from the correct place

--- src/textual_plotext/__init__.py
+++ src/textual_plotext/__init__.py
@@ -1,5 +1,6 @@
 """A Textual widget library for wrapping the Plotext terminal plotting library."""
 
-from .plotext_plot import Plot, PlotextPlot
+from .plot import Plot
+from .plotext_plot import PlotextPlot
 
 __all__ = ["Plot", "PlotextPlot"]
