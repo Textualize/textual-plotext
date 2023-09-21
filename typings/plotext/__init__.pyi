2ccbe58a963f0d4adef0b0285b2c84de514f2034
Dave Pearson <davep@davep.org>
2023-09-21 22:13:39 +0100

    Tweak the stubs to make them tidier

--- typings/plotext/__init__.pyi
+++ typings/plotext/__init__.pyi
@@ -5,22 +5,24 @@ the code of this library; but as time goes on this could turn into a set of
 stubs for the whole of Plotext.
 """
 
-from typing import Any, Union, Tuple
+from __future__ import annotations
+
+from typing import Any
 
 def sin(
-    periods: int = 2,
-    length: int = 200,
-    amplitude: int = 1,
-    phase: float = 0,
-    decay: int = 0,
+    periods: int,
+    length: int,
+    amplitude: int,
+    phase: float,
+    decay: int,
 ) -> list[float]: ...
-def square(periods: int = 2, length: int = 200, amplitude: float = 1) -> list[int]: ...
+def square(periods: int, length: int, amplitude: float) -> list[int]: ...
 def colorize(
     string: str,
-    fullground: Union[str, int, Tuple[int, int, int], None] = None,
-    style: str | None = None,
-    background: Union[str, int, Tuple[int, int, int], None] = None,
-    show: bool = False,
+    fullground: str | int | tuple[int, int, int] | None,
+    style: str | None,
+    background: str | int | tuple[int, int, int] | None,
+    show: bool,
 ) -> str: ...
 def uncolorize(string: str) -> str: ...
 def transpose(data: list[list[Any]]) -> list[list[Any]]: ...
