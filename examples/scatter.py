c7f1a3fcb924151b42bf401cb7d834a74084426e
Dave Pearson <davep@davep.org>
2023-10-02 19:23:26 +0100

    Add docstrings to the small scatter example

--- examples/scatter.py
+++ examples/scatter.py
@@ -1,13 +1,19 @@
+"""A small self-contained example of a Plotext plot in a Textual app."""
+
 from textual.app import App, ComposeResult
 
 from textual_plotext import PlotextPlot
 
 
 class ScatterApp(App[None]):
+    """Example Textual application showing a Plotext plot."""
+
     def compose(self) -> ComposeResult:
+        """Compose the plotting widget."""
         yield PlotextPlot()
 
     def on_mount(self) -> None:
+        """Set up the plot."""
         plt = self.query_one(PlotextPlot).plt
         plt.title("Scatter Plot")
         plt.scatter(plt.sin())
